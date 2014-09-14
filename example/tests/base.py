86436e6c76340a5d1b49228c3e40a256faf53f28
st4lk <myhappydo@gmail.com>
2014-09-14 15:31:12 +0400

    fix tests for django 1.7 (related to django 21688), refs #7

--- example/tests/base.py
+++ example/tests/base.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import clear_url_caches
+from django.utils import translation
 try:
     from django.test.utils import TransRealMixin
 except ImportError:
@@ -17,4 +18,11 @@ class URLTestCaseBase(TransRealMixin, TestCase):
     def tearDown(self):
         # Make sure we will leave an empty cache for other testcases.
         clear_url_caches()
+        # Not sure why exactly, but TransRealMixin was removied in django 1.7
+        # look https://github.com/django/django/commit/b87bc461c89f2006f0b27c7240fb488fac32bed1
+        # Without it, tests will fail with django 1.7, because language
+        # will be kept in _active.value between tests.
+        # have to delete _active.value explicitly by calling deactivate
+        # TODO: investigate this problem more deeply
+        translation.deactivate()
         super(URLTestCaseBase, self).tearDown()
