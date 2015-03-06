72420a35ec9ceef90afc163d9bb2328a19a5b20f
st4lk <myhappydo@gmail.com>
2015-03-06 13:15:40 +0300

    fix USE_I18N=False test, refs #14

--- example/tests/test_noni18n_urls.py
+++ example/tests/test_noni18n_urls.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
 from django.test.utils import override_settings
+from django.core.urlresolvers import clear_url_caches
 
 from .base import URLTestCaseBase
 
 
 class Noni18nUrlsTestCase(URLTestCaseBase):
+    urls = 'tests.urls_noni18n'
 
     def test_noni18n_page(self):
         url = reverse('onelang')
         self.assertEqual(url, '/onelang/')
         response = self.client.get(url)
         self.assertContains(response, 'One language content')
+
+
+class SettingsChangeTestCase(URLTestCaseBase):
+
+    def setUp(self):
+        super(URLTestCaseBase, self).tearDown()
+        clear_url_caches()
+        # don't reload urlconf here
 
     @override_settings(USE_I18N=False)
     def test_usei18n_false(self):
