8435edf83c14374b1b996889aeda6b217c39f2a7
st4lk <myhappydo@gmail.com>
2015-04-13 02:24:34 +0300

    test django version at import time only if django avaliable

--- solid_i18n/__init__.py
+++ solid_i18n/__init__.py
@@ -1,11 +1,15 @@
 import warnings
-from django import VERSION
 __author__ = 'st4lk'
 __version__ = '0.9.1'
 
-DEPRECATED_DJANGO_VERSIONS = [(1, 5)]
+try:
+    from django import VERSION
+except ImportError:
+    pass
+else:
+    DEPRECATED_DJANGO_VERSIONS = [(1, 5)]
 
-if VERSION[:2] in DEPRECATED_DJANGO_VERSIONS:
-    warnings.warn("Support of Django versions %s will be dropped in "
-        "1.0 version of solid_i18n" % DEPRECATED_DJANGO_VERSIONS,
-        PendingDeprecationWarning)
+    if VERSION[:2] in DEPRECATED_DJANGO_VERSIONS:
+        warnings.warn("Support of Django versions %s will be dropped in "
+            "1.0 version of solid_i18n" % DEPRECATED_DJANGO_VERSIONS,
+            PendingDeprecationWarning)
