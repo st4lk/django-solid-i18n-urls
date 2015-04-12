d396e965d3971ba2eea497ebb351af69294b2dc5
st4lk <myhappydo@gmail.com>
2015-04-13 01:12:33 +0300

    restore old django versions support with warnings, refs #8, #9

--- solid_i18n/__init__.py
+++ solid_i18n/__init__.py
@@ -1,2 +1,11 @@
+import warnings
+from django import VERSION
 __author__ = 'st4lk'
 __version__ = '0.9.1'
+
+DEPRECATED_DJANGO_VERSIONS = [(1, 5)]
+
+if VERSION[:2] in DEPRECATED_DJANGO_VERSIONS:
+    warnings.warn("Support of Django versions %s will be dropped in "
+        "1.0 version of solid_i18n" % DEPRECATED_DJANGO_VERSIONS,
+        PendingDeprecationWarning)
